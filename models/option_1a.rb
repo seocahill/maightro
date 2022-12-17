0dbd67a962793e7f8c83c5246b24054f4b300f08
Seosamh Cahill <seo.cahill@gmail.com>
2022-12-17 19:40:55 +0100

    basic working option 1a

--- models/option_1a.rb
+++ models/option_1a.rb
@@ -25,28 +25,45 @@ ballina_trains = []
 westport_trains = all_trips.flatten.select { |t| t.info == "to Westport" }
 dub_trains = all_trips.flatten.select { |t| t.info == "to Dublin Heuston" }
 
+local_idx = 0
+
 westport_trains.each do |wt|
-  from = wt.dep - (30 * 60) # 27 min + 3 dwell/transfer
+  from = wt.dep - (29 * 60) # 27 min + 2 dwell/transfer
   ballina_trains << TrainPath.new(from: 'Ballina', to: "Manulla", dep: from, arr: wt.dep, trip_id: wt.trip_id)
 
   depart_time = wt.dep
   arr_time = depart_time + (27 * 60)
   # trip id is nil here because this train is connection from Dublin, not Maightro
-  ballina_trains << TrainPath.new(from: 'Manulla', to: "Ballina", dep: depart_time, arr: arr_time, trip_id: nil)
+  ballina_trains << TrainPath.new(from: 'Manulla', to: "Ballina", dep: depart_time, arr: arr_time, trip_id: "L-#{local_idx}")
+  local_idx +=1
 end
 
 dub_trains.each do |dt|
-  from = dt.dep - (30 * 60) # 27 min + 3 dwell/transfer
+  from = dt.arr - (29 * 60) # 27 min + 2 dwell/transfer
   # trip id is nil here because this train is connection to Dublin, not Maightro
-  ballina_trains << TrainPath.new(from: 'Ballina', to: "Manulla", dep: from, arr: dt.dep, trip_id: nil)
+  ballina_trains << TrainPath.new(from: 'Ballina', to: "Manulla", dep: from, arr: dt.arr, trip_id: "L-#{local_idx}")
+  local_idx +=1
 
-  depart_time = dt.dep
+  depart_time = dt.arr
   arr_time = depart_time + (27 * 60)
   ballina_trains << TrainPath.new(from: 'Manulla', to: "Ballina", dep: depart_time, arr: arr_time, trip_id:  dt.trip_id)
 end
 
-rows = (ballina_trains + westport_trains + dub_trains).map(&:values)
-headers = %w[path connection dep arr dwell]
+rows = (ballina_trains + westport_trains + dub_trains)
+          .group_by(&:trip_id)
+          .map do |g,t|
+            if t.length == 2 && t.first.trip_id.include?("C-")
+              bt, wt = t
+              [bt.from, wt.to, bt.dep.strftime("%H:%M"), wt.arr.strftime("%H:%M"), (wt.arr - bt.dep).fdiv(60).round, bt.trip_id]
+            elsif t.length == 2 && t.first.trip_id.include?("R-")
+              bt, dt = t
+              [dt.from, bt.to, dt.dep.strftime("%H:%M"), bt.arr.strftime("%H:%M"), (bt.arr - dt.dep).fdiv(60).round, dt.trip_id]
+            else
+              # [t.first.from, t.first.to, t.first.dep.strftime("%H:%M"), t.first.arr.strftime("%H:%M"), (t.first.arr - t.first.dep).fdiv(60).round, t.first.trip_id]
+            end
+          end.compact.sort_by { |t| t[2] }
+
+headers = %w[path connection dep arr duration dwell]
 puts Terminal::Table.new rows: rows, headings: headers, title: 'An Maightró', style: { all_separators: true }
 
 # ba = ballina_trains.select { |t| t.from == 'Ballina-Manulla' }
