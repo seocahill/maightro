3a4c9975de69aa6aeddc1142669b1acfa37bcb42
Seosamh Cahill <seo.cahill@gmail.com>
2022-12-26 23:47:28 +0100

    fix default values

--- maightro.rb
+++ maightro.rb
@@ -10,11 +10,15 @@ Dir.glob('./models/**/*.rb').each { |file| require file }
 get '/' do
   @timetables = []
   @default_date = Date.today.strftime "%Y-%m-%d"
+  @default_session = "Option1"
   erb :index
 end
 
 post '/timetable' do
   @timetables = []
+  @default_date = params['date']
+  @default_scenario = params["scenario"]
+
   @timetables = if params["scenario"]
                   date = params["date"].split('-').join
                   [Module.const_get(params["scenario"]).new(date).rows]
