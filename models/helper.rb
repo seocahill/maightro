26dc4a02dbe4e7b65482b77e7cd55afc67457a0e
Seosamh Cahill <seo.cahill@gmail.com>
2022-12-31 01:34:05 +0100

    fix: added accuration time generation
    hopeful will cut out alot of the janky code

--- models/helper.rb
+++ models/helper.rb
@@ -21,27 +21,51 @@ module Helper
   #   scope :disabled, -> { where(disabled: true) }
   # end
 
-  def stops(row)
-    bw = [
-        ["Foxford", (Time.parse(row[2]) + (16*60)).strftime("%H:%M")],
-        ["Manulla", (Time.parse(row[2]) + (27*60)).strftime("%H:%M")],
-        ["Castlebar", (Time.parse(row[2]) + (36*60)).strftime("%H:%M")],
-      ]
-    wb = [
-      ["Castlebar", (Time.parse(row[2]) + (14*60)).strftime("%H:%M")],
-      ["Manulla", (Time.parse(row[2]) + (20*60)).strftime("%H:%M")],
-      ["Foxford", (Time.parse(row[2]) + (31*60)).strftime("%H:%M")],
-      ]
-    if row[0] == "Ballina" && row[1] == "Westport"
-      bw
-    elsif row[1] == "Ballina" && row[0] == "Westport"
-      wb
-    elsif row[0] == "Ballina" && row[1] == "Castlebar"
-      bw[0..1]
-    elsif row[1] == "Ballina" && row[0] == "Castlebar"
-      wb[1..2]
+  def find_route(from, to)
+    nephin = ["Ballina", "Foxford", "Manulla Junction", "Castlebar", "Westport"]
+    covey = ["Westport", "Castlebar", "Manulla Junction", "Claremorris", "Ballyhaunis"]
+    costello = ["Ballyhaunis", "Claremorris", "Manulla Junction", "Foxford", "Ballina"]
+
+    [nephin, covey, costello, nephin.reverse, covey.reverse, costello.reverse].each do |r|
+      next unless sdx = r.index(from)
+      next unless edx = r[sdx..].index(to)
+      return r[sdx..edx]
     end
   end
+
+  def stops(from, to, dep)
+    results = [[from, dep]]
+    current = dep
+    find_route(from, to).each_cons(2) do |f,t|
+      next unless f && t
+
+      current += (@stop_info[f][t] + @dwell)
+      results << [t, current]
+    end
+    results
+  end
+
+  # def stops(row)
+  #   bw = [
+  #       ["Foxford", (Time.parse(row[2]) + (16*60)).strftime("%H:%M")],
+  #       ["Manulla", (Time.parse(row[2]) + (27*60)).strftime("%H:%M")],
+  #       ["Castlebar", (Time.parse(row[2]) + (36*60)).strftime("%H:%M")],
+  #     ]
+  #   wb = [
+  #     ["Castlebar", (Time.parse(row[2]) + (14*60)).strftime("%H:%M")],
+  #     ["Manulla", (Time.parse(row[2]) + (20*60)).strftime("%H:%M")],
+  #     ["Foxford", (Time.parse(row[2]) + (31*60)).strftime("%H:%M")],
+  #     ]
+  #   if row[0] == "Ballina" && row[1] == "Westport"
+  #     bw
+  #   elsif row[1] == "Ballina" && row[0] == "Westport"
+  #     wb
+  #   elsif row[0] == "Ballina" && row[1] == "Castlebar"
+  #     bw[0..1]
+  #   elsif row[1] == "Ballina" && row[0] == "Castlebar"
+  #     wb[1..2]
+  #   end
+  # end
 
 
 
@@ -54,7 +78,7 @@ module Helper
 
     def find_station(current, stations)
       index = current['locX']
-      stations.dig(index, 'name')
+      stations.dig(index, 'name').split.first
     end
   end
 end
---
+++ models/scenarios/base_option.rb
@@ -0,0 +1,81 @@
+#! /usr/bin/ruby
+# frozen_string_literal: true
+
+# Status quo
+
+require 'json'
+# require 'pry'
+require 'time'
+require 'terminal-table'
+require_relative '../helper'
+require_relative '../journey_planner'
+require_relative '../train_path'
+
+class BaseOption
+  include Helper
+
+=begin
+  # structure
+  "secL"=>
+   [{"type"=>"JNY",
+     "jny"=>
+      {
+       "stopL"=>
+        [{"locX"=>0, "dTimeS"=>"093500", "dDirTxt"=>"Manulla Junction"},
+         {"locX"=>2, "aTimeS"=>"094600",  "dTimeS"=>"094600"},
+         {"locX"=>1, "aTimeS"=>"100200", "aProgType"=>"PROGNOSED"}],
+=end
+  def initialize
+    @stop_info = YAML.load(File.read("config.yaml"))
+    @turnaround = 180.0 # min 3 minutes to reverse direction
+    @dwell = 60.0 # max of arr - dep when present
+    binding.pry
+  end
+
+  def generate_configuration
+    date = Time.now.strftime "%Y%m%d"
+
+    # nephin
+    nephin_line = JourneyPlanner.new.search(date, "Ballina", "Westport")
+    nephin_line.trains_out.each do |trip|
+      extract_trip_durations(trip, nephin_line.stations)
+    end
+    nephin_line.trains_ret.each do |trip|
+      extract_trip_durations(trip, nephin_line.stations)
+    end
+
+    # covey
+    covey_line = JourneyPlanner.new.search(date, "Westport", "Claremorris")
+    covey_line.trains_out.each do |trip|
+      extract_trip_durations(trip, covey_line.stations)
+    end
+    covey_line.trains_ret.each do |trip|
+      extract_trip_durations(trip, covey_line.stations)
+    end
+
+    # write config
+    File.open('config.yaml', 'w') do |file|
+      file.write(@stop_info.to_h.to_yaml)
+    end
+  end
+
+  private
+
+  def extract_trip_durations(trip, stations)
+    trip.dig('secL').each do |t|
+      t.dig('jny', 'stopL').each_cons(2) do |start, finish|
+        next unless  finish['aTimeS'] && start["dTimeS"]
+
+        dur = parse_time(finish['aTimeS']) - parse_time(start["dTimeS"])
+        if start['aTimeS'] && start["dTimeS"]
+          puts parse_time(start["dTimeS"]) - parse_time(start['aTimeS']) # dwell
+        end
+        x = @stop_info[find_station(start, stations)] ||= {}
+        y = x[find_station(finish, stations)] ||= nil
+        x[find_station(finish, stations)] = dur if (y.nil? || dur < y)
+      end
+    end
+  end
+end
+
+BaseOption.new.generate_configuration if __FILE__ == $PROGRAM_NAME
