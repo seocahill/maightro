78c18aa8384042fbd76196cdd234ac70555c2704
Seosamh Cahill <seo.cahill@gmail.com>
2023-01-03 22:36:55 +0100

    refactor: make option code modular for reuse

--- models/scenarios/base_option.rb
+++ models/scenarios/base_option.rb
@@ -25,10 +25,19 @@ class BaseOption
          {"locX"=>2, "aTimeS"=>"094600",  "dTimeS"=>"094600"},
          {"locX"=>1, "aTimeS"=>"100200", "aProgType"=>"PROGNOSED"}],
 =end
-  def initialize
+  def initialize(date = '20221222', from = 'Ballina', to = 'Westport', sort = 'dep')
     @stop_info = YAML.load(File.read("config.yaml"))
-    @turnaround = 180.0 # min 3 minutes to reverse direction
-    @dwell = 60.0 # max of arr - dep when present
+    @dwell = 60.0
+    @turnaround = @dwell * 3
+    @date = date
+    @sort = sort
+    @from = from
+    @to = to
+    exec_option
+  end
+
+  def exec_option
+    raise "Implement me"
   end
 
   def generate_configuration
@@ -56,6 +65,34 @@ class BaseOption
     File.open('config.yaml', 'w') do |file|
       file.write(@stop_info.to_h.to_yaml)
     end
+  end
+
+  def rows
+    results = []
+    # find route group from search terms: nephin, covey, costello
+    routes, _stops = find_route(@from, @to)
+    # seach can return multiple routes
+    routes.each do |route|
+      # group by group trip ids
+      rows = @train_trips.reject { |t| t.send("#{route}_id").nil? }.group_by(&:"#{route}_id").map do |trip_id, trains|
+        # make this a hash instead, hashes are sorted in ruby so this will work
+        stops = trains.flat_map { |train| train.stops }.sort_by { |stop| stop[1] }.each_with_object({}) { |i, obj| obj[i[0]] = i[1] } # join all stops and sort by time
+        # filter by @from @to and add other tt info
+        next unless stops[@from] && stops[@to]
+        next unless stops[@from] < stops[@to]
+        # return train result for tt
+        [@from, @to, stops[@from].strftime("%H:%M"), stops[@to].strftime("%H:%M"), trains.map(&:info).join('; '), trains.first.send("#{route}_id")]
+      end.compact
+      # search will return dups for certain sections, uniuque
+      rows.each { |row| results << row unless results.any? { |res| res[5] == row[5] } }
+    end
+    results
+  end
+
+  def as_ascii
+    headers = %w[from to dep arr info trip_id]
+    sort = headers.index(@sort)
+    puts Terminal::Table.new rows: rows.sort_by { |r| r[sort] }, headings: headers, title: 'An Maightró', style: { all_separators: true }
   end
 
   private
