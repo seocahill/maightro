29a2e6ab60ba3f9254d16ad9d97565a9f63ed65d
Seosamh Cahill <seo.cahill@gmail.com>
2023-01-07 17:51:48 +0100

    feature: add sentry

--- maightro.rb
+++ maightro.rb
@@ -1,8 +1,22 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-# require 'sentry-ruby'
+require 'sentry-ruby'
 require 'date'
+
+# catch bugs
+Sentry.init do |config|
+  config.dsn = ENV['SENTRY_DSN']
+  config.breadcrumbs_logger = [:sentry_logger, :http_logger]
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    0.5
+  end
+end
 
 # pull in the helpers and controllers
 Dir.glob('./models/**/*.rb').each { |file| require file }
@@ -24,6 +38,10 @@ end
 
 get '/about' do
   erb :about
+end
+
+get '/test-sentry'
+  Sentry.capture_message("test message")
 end
 
 post '/timetable' do
